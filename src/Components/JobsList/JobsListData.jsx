b8769905b385950ca3d5fa89913cdc7055d592a9
Rodina <Rodinamobark3@gmail.com>
2024-05-02 07:45:47 +0300

    job details

--- src/Components/JobsList/JobsListData.jsx
+++ src/Components/JobsList/JobsListData.jsx
@@ -1,107 +1,106 @@
 export const JobsListData = [
     {
-    jobId :"1",
-    jobTitle : "Senior Android",
-    postDate: "24/4/2024",
-    jobType: "Full time",
-    jobSalary: "200$",
-    jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-    },
-    ,
-    {
-        jobId :"2",
-        jobTitle : "Senior Android",
+        jobId: "1",
+        jobTitle: "Senior Android",
         postDate: "24/4/2024",
         jobType: "Full time",
         jobSalary: "200$",
         jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-        },
-        {
-            jobId :"3",
-            jobTitle : "Senior Android",
-            postDate: "24/4/2024",
-            jobType: "Full time",
-            jobSalary: "200$",
-            jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-            },
-            {
-                jobId :"4",
-                jobTitle : "Senior Android",
-                postDate: "24/4/2024",
-                jobType: "Full time",
-                jobSalary: "200$",
-                jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                },
-                {
-                    jobId :"5",
-                    jobTitle : "Senior Android",
-                    postDate: "24/4/2024",
-                    jobType: "Full time",
-                    jobSalary: "200$",
-                    jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                    },
-                    {
-                        jobId :"6",
-                        jobTitle : "Senior Android",
-                        postDate: "24/4/2024",
-                        jobType: "Full time",
-                        jobSalary: "200$",
-                        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                        },
-                        {
-                            jobId :"7",
-                            jobTitle : "Senior Android",
-                            postDate: "24/4/2024",
-                            jobType: "Full time",
-                            jobSalary: "200$",
-                            jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                            },
-                            {
-                                jobId :"8",
-                                jobTitle : "Senior Android",
-                                postDate: "24/4/2024",
-                                jobType: "Full time",
-                                jobSalary: "200$",
-                                jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                                },
-                                {
-                                    jobId :"9",
-                                    jobTitle : "Senior Android",
-                                    postDate: "24/4/2024",
-                                    jobType: "Full time",
-                                    jobSalary: "200$",
-                                    jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                                    },
-                                    {
-                                        jobId :"10",
-                                        jobTitle : "Senior Android",
-                                        postDate: "24/4/2024",
-                                        jobType: "Full time",
-                                        jobSalary: "200$",
-                                        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                                        },
-                                        {
-                                            jobId :"11",
-                                            jobTitle : "Senior Android",
-                                            postDate: "24/4/2024",
-                                            jobType: "Full time",
-                                            jobSalary: "200$",
-                                            jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                                            },
-                                            {
-                                                jobId :"12",
-                                                jobTitle : "Senior Android",
-                                                postDate: "24/4/2024",
-                                                jobType: "Full time",
-                                                jobSalary: "200$",
-                                                jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                                                },
-                                                {
-                                                    jobId :"13",
-                                                    jobTitle : "Senior Android",
-                                                    postDate: "24/4/2024",
-                                                    jobType: "Full time",
-                                                    jobSalary: "200$",
-                                                    jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
-                                                    },
+    },
+    {
+        jobId: "2",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "3",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "4",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "5",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "6",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "7",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "8",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "9",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "10",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "11",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "12",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
+    {
+        jobId: "13",
+        jobTitle: "Senior Android",
+        postDate: "24/4/2024",
+        jobType: "Full time",
+        jobSalary: "200$",
+        jobDescription: "A job description is an employer document that describes an open role at the company. It’s often included in job ads to give prospective candidates a clear idea of a role’s scope and what skills and experience are required to succeed in it. It also typically includes a summary of the position, job title, and information about the company culture and benefits."
+    },
 ]
--- src/Components/ReviewProposels/ReviewProposels.jsx
+++ src/Components/ReviewProposels/ReviewProposels.jsx
@@ -1,13 +1,64 @@
 import React from 'react'
-
+import  { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { JobsListData } from '../JobsList/JobsListData';
+import './ReviewProposels.css'
 function ReviewProposels() {
+ const [Job, setJob] = useState(null);
+ const { jobId } = useParams();
+    useEffect(() => {
+       /* const job = JobsListData.find(item => item.jobId === jobId);
+        setJob(JobsListData.find(item => item.jobId === jobId))*/
+      fetchData();
+    }, [jobId]);
+
+    const fetchData = async () => {
+      console.log('Fetching data...');
+    
+        try {
+            const baseurl='https://jobconnectapi-1.onrender.com/jobs/${jobId}'
+            const response = await fetch( baseurl, {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjI5YzVhN2RmLWE5M2MtNGVmNi1iMzUwLTEzYTliYzY3M2U3MyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkpvYlNlZWtlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2dpdmVubmFtZSI6IkpvYlNlZWtlcjEiLCJleHAiOjE3MTQ2NDY4OTIsImlzcyI6ImpvYkNvbm5lY3QifQ.OG_EHO82A23dcZvXRNofllgpdlLSVHCHX5dY_dA5JY8'
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const jsonData = await response.json();
+            setJob(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
   return (
      <div>
-      
-        <div className='job-list-container'>
+    <div className='job-list-container'>
       <div className='logo-container'><text className='job-connect-logo'>JobConnect</text></div>  
     </div>
-    
+    <div className='job-details-header'>Job details</div> 
+    <div>
+    <div className='job-details-card' >
+       {Job && (
+                    <>
+                        <p className='job-title'>{Job.jobTitle}</p>
+                        <p className='job-date'>{Job.postDate}</p>
+                        <div className='job-type-div'>
+                        <p className='job-type-text'>Job Type:</p>
+                        <p className='job-type'>{Job.jobType}</p> 
+                        </div>
+                        <div className='job-description-div' >
+                        <p className='job-description-text'>Job description:</p>
+                        <p className='job-description-deails'>{Job.jobDescription}</p>
+                        </div>
+            
+                    </>
+                )}
+      </div>
+    </div>
+    <div></div>
+
      </div>
   )
 }
