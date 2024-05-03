8bce61cd5e999ea83abf10a133a1019f7a81b38b
Rodina <Rodinamobark3@gmail.com>
2024-05-03 09:40:51 +0300

    view proposal edit

--- src/Components/ReviewProposels/ReviewProposels.jsx
+++ src/Components/ReviewProposels/ReviewProposels.jsx
@@ -89,8 +89,7 @@ function ReviewProposels() {
             <p className='user-name'>{val.jobSeeker.userName}</p>
             <p className='user-email'>{val.jobSeeker.email}</p>
             <div className='buttons-container'>
-                <Link to="/viewProposel:${val.ProposalId}"><button className='view-proposel-button'>View proposel</button></Link>
-            <button className='view-proposel-button'>View proposel</button>
+                <Link to='/viewProposel:${val.ProposalId}'><button className='view-proposel-button'>View proposel</button></Link>
             </div>
             </div>
         )
