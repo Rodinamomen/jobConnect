78e6d4dbee358a875b155a5c508813d69208774e
Rodina <Rodinamobark3@gmail.com>
2024-05-05 08:55:37 +0300

    done i guess

--- src/Components/JobsList/JobsList.jsx
+++ src/Components/JobsList/JobsList.jsx
@@ -6,7 +6,7 @@ import NavGraph from '../NavGraph/NavGraph';
 
 function JobList(props) {
   const [allJobs, setAllJobs] = useState(null);
-  const token = localStorage.getItem('token');
+ //const token = localStorage.getItem('token');
   useEffect(() => {
       fetchData();
   }, []);
@@ -14,10 +14,10 @@ function JobList(props) {
   const fetchData = async () => {
     console.log('Fetching data...');
       try {
-          const response = await fetch('http://localhost:5109/employer/jobs', {
+          const response = await fetch('https://jobconnectapi-1.onrender.com/employer/jobs', {
               method: 'GET',
               headers: {
-                  'Authorization': `Bearer ${token}`,
+                  'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjIzZjljNzJkLTIyYTQtNDQzNi04MDdmLWJkODJkMGFlMWY3NSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkVtcGxveWVyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZ2l2ZW5uYW1lIjoiRW1wbG95ZXIyIiwiZXhwIjoxNzE0OTcxOTkwLCJpc3MiOiJqb2JDb25uZWN0In0.5Jd6RxCvg9YSdtPrEQ47SJim7ga8U6ybGDA8QHBeVRY`,
                    'Content-Type': 'application/json'
               }
           });
@@ -38,7 +38,7 @@ function JobList(props) {
         
         </div>
         <div className='job-cards-container'>
-        {JobsListData && JobsListData.map((val,key)=>{
+        {allJobs && allJobs.map((val,key)=>{
         return(
          <div className='job-card' key={key} >
            <p className='job-title'>{val.jobTitle}</p>
